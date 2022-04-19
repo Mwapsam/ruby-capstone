9ef1c805da13962c6719a0ada454c108a67d707c
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-04-19 13:12:57 +0400

    Created app file and added options in main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,72 @@
-def start
-  puts '******** WELCOME TO THE RUBY CAPSTONE *********'
+require_relative './app/app'
+
+def menu
+  puts
+  puts 'Please choose an option by entering a number'
+
+  options = [
+    'List all the books',
+    'List all the music albums',
+    'List all the games',
+    'List all the genres',
+    'List all the labels',
+    'List all the authors',
+    'Add a book',
+    'Add a music album',
+    'Add a game',
+    'Add a genre',
+    'Add an label',
+    'Add a author',
+    'Exit'
+  ]
+
+  options.each_with_index do |option, index|
+    puts "#{index + 1} - #{option}"
+  end
 end
 
-start
+def console_start
+  app = App.new
+  app.WELCOME
+
+  while menu
+    selected = gets.chomp.to_i
+    case selected
+    when 1
+      puts 'app.list_books'
+    when 2
+      puts 'app.list_music_albums'
+    when 3
+      puts 'app.list_games'
+    when 4
+      puts 'app.list_genres'
+    when 5
+      puts 'app.list_labels'
+    when 6
+      puts 'app.list_authors'
+    when 7
+      puts 'app.add_book'
+    when 8
+      puts 'app.add_music_albums'
+    when 9
+      puts 'app.add_games'
+    when 10
+      puts 'app.add_genres'
+    when 11
+      puts 'app.add_labels'
+    when 12
+      puts 'app.add_author'
+    when 13
+      puts 'Thank youu for usinf our app!'
+      break
+    else
+      puts 'Please choose an appropriate number'
+    end
+  end
+end
+
+def main
+  console_start
+end
+
+main
