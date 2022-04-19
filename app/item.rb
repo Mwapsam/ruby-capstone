36a7108450ad28a95d2ef2a29dd04f979b4a829d
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-04-19 17:06:15 +0400

    Update item.rb

--- app/item.rb
+++ app/item.rb
@@ -2,13 +2,14 @@ class Item
   attr_accessor :published_date, :name
   attr_reader :id, :archived, :genre
 
+  # rubocop:disable Style/OptionalBooleanParameter
   def initialize(name, published_date, archived = false, id = Random.rand(1..1000))
     @id = id
     @name = name
-    @published_date = published_date 
+    @published_date = published_date
     @archived = archived
   end
-
+ # rubocop:enable Style/OptionalBooleanParameter
   def add_genre=(genre)
     @genre = genre
     genre.items.push(self) unless genre.items.include?(self)
