d605e90bd2f2619256abd7612b64dbd9bc95d5bb
Billions Joel <joelatugonza@gmail.com>
2022-04-20 12:16:59 +0300

    Define constructor

--- game-and-author/author.rb
+++ game-and-author/author.rb
@@ -4,7 +4,13 @@ class Author < Item
 
   attr_accessor :id, :first_name, :last_name, :items
 
-
+  def initialize(id, first_name, last_name, items)
+    super
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = items
+  end
 
 
 end
