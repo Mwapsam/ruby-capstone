186587393988c73d6a2874b69756cc0dff1c86b7
Billions Joel <joelatugonza@gmail.com>
2022-04-20 12:17:52 +0300

    Define constructor

--- game-and-author/game.rb
+++ game-and-author/game.rb
@@ -1,7 +1,10 @@
 class Game
   attr_accessor :multiplayer, :last_played_at
 
-
+  def initialize(multiplayer, last_played_at)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
 
 end
