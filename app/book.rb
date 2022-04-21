56d3a9de553cd0856911337a4e0a6459c93c57eb
Mwapsam <mwapsam@gmail.com>
2022-04-21 16:30:42 +0200

    Fixed bugs on book methods

--- app/book.rb
+++ app/book.rb
@@ -1,10 +1,10 @@
 require_relative '.\item'
 
 class Book < Item
-  attr_accessor :name, :publish_date, :cover_state, :publisher
+  attr_accessor :name, :published_date, :cover_state, :publisher
 
-  def initialize(publisher, cover_state, name, publish_date, id = Random.rand(1..1000))
-    super(name, publish_date, id)
+  def initialize(publisher, cover_state, name, published_date, id = Random.rand(1..1000))
+    super(name, published_date, id)
     @publisher = publisher
     @cover_state = cover_state
   end
