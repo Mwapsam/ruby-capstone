4602c61391a3bc5ba3588b2d7cc00fcfc90c3933
Billions Joel <joelatugonza@gmail.com>
2022-04-21 03:52:29 +0300

    Describe tests for methods

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,5 +9,34 @@ def create_a_game(last_played_year = 2000, publish_year = 2000)
     id: 1000
   )
 end
+describe Game do
+  it 'move game to archive unless publish date is more than 10 years' do
+    publish_year = DateTime.now.year.to_i - 10
+    last_played_year = DateTime.now.year.to_i - 3
+    game = create_a_game(last_played_year, publish_year)
+    game.move_to_archive
+    is_archived = game.archived
+    expect(is_archived).to eq false
+  end
 
+describe 'last publish date more than 10 years ago' do
+    before :all do
+      @publish_year = DateTime.now.year.to_i - 11
+    end
+    it 'moves game to archive if last played more than 2 years ago' do
+      last_played_year = DateTime.now.year.to_i - 3
+      game = create_a_game(last_played_year, @publish_year)
+      game.move_to_archive
+      is_archived = game.archived
+      expect(is_archived).to eq true
+    end
+
+    it 'should not move game to archive if game was last played more than 2 years ago' do
+      last_played_year = DateTime.now.year.to_i - 2
+      game = create_a_game(last_played_year, @publish_year)
+      game.move_to_archive
+      is_archived = game.archived
+      expect(is_archived).to eq false
+    end
+  end
 end
