d1d2cb29182a6f77690d6fa297d07ce392039054
Mwapsam <mwapsam@gmail.com>
2022-04-21 14:53:42 +0200

    Run tests for book and label methods

--- data/schema.sql
+++ data/schema.sql
@@ -14,3 +14,17 @@ CREATE TABLE music_albums (
     genre_id INT,
     FOREIGN KEY (genre_id) REFERENCES genres(id)
 );
+
+CREATE TABLE books(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  publisher VARCHAR(255),
+  cover_state VARCHAR(255),
+  FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE labels(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255),
+  color VARCHAR(255),
+  PRIMARY KEY(id)
+);
