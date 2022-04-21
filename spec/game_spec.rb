c7a03e14837afa2bfe75719fca239bdd85bcc88c
Billions Joel <joelatugonza@gmail.com>
2022-04-21 03:47:13 +0300

    Instantiate game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,5 +2,12 @@ require_relative '../classes/game'
 require 'date'
 
 def create_a_game(last_played_year = 2000, publish_year = 2000)
+  Game.new(
+    multiplayer: true,
+    last_played_at: DateTime.parse("#{last_played_year}-01-01"),
+    published_date: DateTime.parse("#{publish_year}-01-01"),
+    id: 1000
+  )
+end
 
 end
