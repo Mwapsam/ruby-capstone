66492ae32d682d26647af5e61b2ef8e17e3121ed
Billions Joel <joelatugonza@gmail.com>
2022-04-21 03:19:41 +0300

    Add att reader and parameters

--- game-and-author/author.rb
+++ game-and-author/author.rb
@@ -1,17 +1,18 @@
 require_relative '../app/item'
 
 class Author < Item
-  attr_accessor :id, :first_name, :last_name, :items
+  attr_accessor :id, :first_name, :last_name
+  attr_reader :items
 
-  def initialize(id, first_name, last_name, items)
+  def initialize(id: rand(1000), first_name:, last_name:)
     super
     @id = id
     @first_name = first_name
     @last_name = last_name
-    @items = items
+    @items = []
   end
 
   def add_item(item)
-    @items = item
+    @items << item
   end
 end
