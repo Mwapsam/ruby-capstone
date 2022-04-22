77fec7f60ccd3e5883f651d63fd1f7df181a0a96
Billions Joel <joelatugonza@gmail.com>
2022-04-22 11:23:48 +0300

    move schema file to sql folder and add game and author tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -28,3 +28,25 @@ CREATE TABLE labels(
   color VARCHAR(255),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE author(
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        first_name TEXT,
+        last_name TEXT
+);
+
+CREATE TABLE game(
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        multiplayer BOOLEAN,
+        last_played_at DATE,
+        publish_date DATE,
+        archived BOOLEAN,
+        genre_id INTEGER,
+        source_id INTEGER,
+        author_id INTEGER,
+        label_id INTEGER,
+        FOREIGN KEY(genre_id) REFERENCES genre(id),
+        FOREIGN KEY(source_id) REFERENCES source(id),
+        FOREIGN KEY(author_id) REFERENCES author(id),
+        FOREIGN KEY(label_id) REFERENCES label(id)
+);
