9854aa577062887c485908c09f448fa9baf9d55d
Billions Joel <joelatugonza@gmail.com>
2022-04-23 00:14:23 +0300

    Fix linter

--- data/games_crud.rb
+++ data/games_crud.rb
@@ -8,27 +8,14 @@ def load_games
   if games_json.empty?
     games_data
   else
-    # JSON.parse(games_json).each do |json|
-    #  game_item = Game.new(json['multiplayer'],DateTime.parse(json['last_played_at']),DateTime.parse(json['published_date']),json['id'])
-    #  games_data.push(game_item)
-    # end
     JSON.parse(games_json).each do |json|
       games_data << Game.from_json(json)
     end
-
   end
   games_data
 end
 
 def save_game(games)
-  # games_data = games.map do |game|
-  #  {
-  #    multiplayer: game[:multiplayer],
-  #    last_played_at: game[:last_played_at],
-  #    published_date: game[:published_date],
-  #  }
-  # end
-
   games_data = JSON.generate(games)
   File.write('./data/games.json', games_data)
 end
